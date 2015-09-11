2cd58f217ccc2e05152e9071ca3e10d37d016dc9
Leonard Ngwa <ngwaleonard@yahoo.com>
2015-09-11 19:19:02 +0000

    Modified ..._create_recipes.rb

--- test/models/recipe_test.rb
+++ test/models/recipe_test.rb
@@ -3,11 +3,16 @@ require 'test_helper'
 class RecipeTest < ActiveSupport::TestCase
   
   def setup
-    @recipe = Recipe.new(Name: "chicken parm", summary: "this is the best chicken parm recipe ever", description: "heat oil, add onions, add tomato sauce, add chicken, cook for 20 minutes")
+    @recipe = Recipe.new(name: "peanutsoup", summary: "this is the best peanut soup recipe ever", description: "heat oil, add onions, add tomato sauce, add chicken and ground peanut, cook for 20 minutes")
   end
   
   test "recipe should be valid" do
     assert @recipe.valid?
+  end
+  
+  test "name should be valid" do
+    @recipe.name = ""
+    assert_not @recipe.valid?
   end
   
 end
