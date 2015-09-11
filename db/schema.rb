6549cbaed4668370b1b14b90bc3ab1fd92a2bc04
Leonard Ngwa <ngwaleonard@yahoo.com>
2015-09-11 05:21:08 +0000

    Updates to chef.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150908182430) do
+ActiveRecord::Schema.define(version: 20150911051425) do
+
+  create_table "chefs", force: :cascade do |t|
+  end
 
   create_table "recipes", force: :cascade do |t|
     t.string   "names"
