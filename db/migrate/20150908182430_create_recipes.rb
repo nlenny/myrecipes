1409307eb9afd36160f2e27f7ec9efb7318f37f3
Leonard Ngwa <ngwaleonard@yahoo.com>
2015-09-11 19:47:00 +0000

    Updates chefs and recipes tables again

--- db/migrate/20150908182430_create_recipes.rb
+++ db/migrate/20150908182430_create_recipes.rb
@@ -1,7 +1,6 @@
 class CreateRecipes < ActiveRecord::Migration
   def change
     create_table :recipes do |t|
-      
       t.string :name
       t.text :summary
       t.text :description
--- db/migrate/20150911051425_create_chefs.rb
+++ db/migrate/20150911051425_create_chefs.rb
@@ -1,7 +1,6 @@
 class CreateChefs < ActiveRecord::Migration
   def change
     create_table :chefs do |t|
-      
       t.string :chefname
       t.string :email
       t.timestamps
