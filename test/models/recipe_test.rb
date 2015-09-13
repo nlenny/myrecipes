679342a18a95cbf9ed135cfcb2cd86cf91fa3400
Leonard Ngwa <ngwaleonard@yahoo.com>
2015-09-13 03:03:24 +0000

    Made changes to recipe_test.rb. I can't get past my naming of name issues

--- test/models/recipe_test.rb
+++ test/models/recipe_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class RecipeTest < ActiveSupport::TestCase
   
   def setup
-    @recipe = Recipe.new(names: "peanutsoup", summary: "this is the best peanut soup recipe ever", description: "heat oil, add onions, add tomato sauce, add chicken and ground peanut, cook for 20 minutes")
+    @recipe = Recipe.new(name: "peanutsoup", summary: "this is the best peanut soup recipe ever", description: "heat oil, add onions, add tomato sauce, add chicken and ground peanut, cook for 20 minutes")
   end
   
   test "recipe should be valid" do
