ecb0d4f94289f7ba46b678997d5b858db7f0cd3f
Leonard Ngwa <ngwaleonard@yahoo.com>
2015-09-13 02:55:39 +0000

    modified schema.rb

--- db/migrate/20150908182430_create_recipes.rb
+++ db/migrate/20150908182430_create_recipes.rb
@@ -1,7 +1,7 @@
 class CreateRecipes < ActiveRecord::Migration
   def change
     create_table :recipes do |t|
-      t.string :name
+      t.string :names
       t.text :summary
       t.text :description
       t.timestamps
--- test/models/recipe_test.rb
+++ test/models/recipe_test.rb
@@ -3,15 +3,15 @@ require 'test_helper'
 class RecipeTest < ActiveSupport::TestCase
   
   def setup
-    @recipe = Recipe.new(name: "peanutsoup", summary: "this is the best peanut soup recipe ever", description: "heat oil, add onions, add tomato sauce, add chicken and ground peanut, cook for 20 minutes")
+    @recipe = Recipe.new(names: "peanutsoup", summary: "this is the best peanut soup recipe ever", description: "heat oil, add onions, add tomato sauce, add chicken and ground peanut, cook for 20 minutes")
   end
   
   test "recipe should be valid" do
     assert @recipe.valid?
   end
   
-  test "name should be valid" do
-    @recipe.name = ""
+  test "names should be valid" do
+    @recipe.names = ""
     assert @recipe.valid?
   end
   
